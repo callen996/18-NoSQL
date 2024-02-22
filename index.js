8241aaeb86ad7f6a60b698b4a91c70a3c42cdafb
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-21 23:33:07 -0500

    new changes

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-
-
-
-
-app.listen(PORT, () => {
-    console.log(process.env.PORT);
-    console.log(`Example app listening on PORT ${PORT}!`)})
+const db = require('./config/connection');
+const routes = require('./routes');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Example app listening on PORT ${PORT}!`)})
+    });
