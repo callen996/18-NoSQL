10badf52a2b00ba90e14ed9bd456000aae56226c
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-21 22:17:47 -0500

    these are changes

---
+++ routes/appRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThoughts,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/appController');
+
+// /api/applications
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThoughts)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/applications/:applicationId/tags
+router.route('/:thoughtId/tags').post(addReaction);
+
+// /api/applications/:applicationId/tags/:tagId
+router.route('/:thoughtId/reaction/:reactionId').delete(removeReaction);
+
+module.exports = router;
