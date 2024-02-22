b98887ebd64d0f8345cb789160b10e919b5ddd6f
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-21 22:56:28 -0500

    more nesting

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,9 +6,9 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/users').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/users/:userId').get(getSingleUser);
 
 module.exports = router;
